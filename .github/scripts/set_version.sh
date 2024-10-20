185b5c400142d56a1f307f2df9403db47a8b36de
Richard Arends <github@mosibi.nl>
2024-10-20 17:06:56 +0200

    updated set version script: 6

--- .github/scripts/set_version.sh
+++ .github/scripts/set_version.sh
@@ -16,6 +16,11 @@ function Info() {
   Message "INFO" "${MESSAGE}"
 }
 
+function Warning() {
+  local MESSAGE="${1}"
+  Message "WARNGIN" "${MESSAGE}"
+}
+
 function Error() {
   local MESSAGE="${1}"
   Message "ERROR" "${MESSAGE}"
@@ -23,9 +28,16 @@ function Error() {
 }
 
 VERSION_FROM_CHANGELOG=$(grep '^## \[[0-9]' CHANGELOG.md | sed -E 's/^##\ \[([0-9].*)\]\ -.*/\1/g' | head -1)
+VERSION_IN_YAML=$(awk '/^ {4}version:/ {print $NF}' heatpump.yaml)
 
 if [[ ! -z "${VERSION_FROM_CHANGELOG}" ]] && [[ "${VERSION_FROM_CHANGELOG}" =~ ^[0-9].*$ ]]; then
   Info "Version extracted from changelog is ${VERSION_FROM_CHANGELOG}"
+  Info "Current version in heatpump.yaml is ${VERSION_IN_YAML}"
+
+  if [[ "${VERSION_FROM_CHANGELOG}" == "${VERSION_IN_YAML}" ]]; then
+    Info "Versions are the same, nothing has to be done now"
+    exit 0
+  fi
 
   # Check if version already exists
   git describe "${VERSION_FROM_CHANGELOG}" --tags  1>/dev/null 2>/dev/null
@@ -36,4 +48,6 @@ if [[ ! -z "${VERSION_FROM_CHANGELOG}" ]] && [[ "${VERSION_FROM_CHANGELOG}" =~ ^
     sed -i -E "s/(^\ {4}version:)(.*$)/\1 ${VERSION_FROM_CHANGELOG}/g" heatpump.yaml || Error "Could not update version in heatpump.yaml"
     Info "Version in heatpump.yaml is successful updated to ${VERSION_FROM_CHANGELOG}"
   fi
+else
+  Warning "Version extracted from CHANGELOG.md does not seem to be a version tag: ${VERSION_FROM_CHANGELOG}, version will not be updated"
 fi

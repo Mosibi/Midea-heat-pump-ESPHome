da2502ad230354014f8b0b113cb4b27855fc58c0
rysiulg <rysiulg@gmail.com>
2024-11-06 22:54:38 +0100

    Update update_version.py

--- .github/scripts/update_version.py
+++ .github/scripts/update_version.py
@@ -37,7 +37,7 @@ print(f'Updated version to {new_version} in {YAML_FILE}')
 with open(CHANGELOG_FILE, 'r') as f:
     changelog = f.read()
 
-updated_changelog = re.sub(r'\[Unreleased\](.*?)\n##', f'[Unreleased]\n### Changed:\n- \n##[{new_version}]\\1\n##', changelog, flags=re.DOTALL)
+updated_changelog = re.sub(r'\[Unreleased\](.*?)\n##', f'[Unreleased]\n### Changed:\n- \n## [{new_version}]\\1\n##', changelog, flags=re.DOTALL)
 
 with open(CHANGELOG_FILE, 'w') as f:
     f.write(updated_changelog)

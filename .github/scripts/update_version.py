36334cc1ff5c92aefbdb76b3f8e09ecd4f43e0e0
rysiulg <rysiulg@gmail.com>
2024-11-06 22:50:13 +0100

    Update update_version.py

--- .github/scripts/update_version.py
+++ .github/scripts/update_version.py
@@ -9,24 +9,24 @@ YAML_FILE = 'heatpump.yaml'
 # Get the version from command-line arguments
 new_version = sys.argv[1]
 
-# Define the constructor for the !secret tag
-def secret_constructor(loader, node):
-    # Return a placeholder value for the !secret tag
-    return 'SECRET_VALUE'  # You can adjust this if you'd like a different placeholder
+# Define a custom loader to handle unknown tags and leave them untouched
+class IgnoreUnknownTagsLoader(yaml.SafeLoader):
+    def construct_tagged(self, tag, node):
+        # This method is called to process any tagged node. If the tag is one we don't want to process,
+        # we simply return the node value (this way PyYAML doesn't try to process it).
+        if tag in ['!secret', '!lambda']:  # Ignore !secret and !lambda tags
+            return node.value
+        return super().construct_tagged(tag, node)  # Use the default behavior for other tags
 
-# Add the custom constructor for the !secret tag
-yaml.add_constructor('!secret', secret_constructor)
-yaml.add_constructor('!lambda', secret_constructor)
-
-# Update the YAML file
+# Use the custom loader to load the YAML
 with open(YAML_FILE, 'r') as yaml_file:
-    yaml_content = yaml.safe_load(yaml_file)  # Load YAML content into yaml_content
+    yaml_content = yaml.load(yaml_file, Loader=IgnoreUnknownTagsLoader)
 
-# Correct the data reference to 'yaml_content' instead of 'data'
-if 'esphome' in yaml_content:
+# Update the version field under esphome
+if 'esphome' in yaml_content and 'project' in yaml_content['esphome']:
     yaml_content['esphome']['project']['version'] = new_version
 
-# Use the default YAML Dumper to maintain order
+# Use the default YAML Dumper to maintain order and write back the modified YAML
 with open(YAML_FILE, 'w') as yaml_file:
     yaml.safe_dump(yaml_content, yaml_file, default_flow_style=False, sort_keys=False)
 

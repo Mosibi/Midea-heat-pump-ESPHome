88fbab1608e3fd8f3e9bf2a1d7c36f56d2e29cf5
Richard Arends <github@mosibi.nl>
2025-05-31 12:25:58 +0200

    Add support for multiple heat pump models. Major change

---
+++ DEVELOPMENT.md
@@ -0,0 +1,56 @@
+
+# Development
+
+First of all, thanks for checking this file, it means that your are interested in making changes to this project, which in the end, will help us all !
+
+## Model support
+
+The model yaml files are created using a Python script that reads the file `source/heatpump-base.yaml` and a model file from the directory `sources/models/`. The changes listed in the model file are applied against `source/heatpump-base.yaml` and written to the `models/` directory with the same name as the original model file. So a file called `source/modes/myfile.yaml` will result in `models/myfile.yaml`
+
+A model file can be used to `modify`, `remove` and `add` a register. The syntax of a model file is as follow:
+
+```yaml
+<change_type>:
+  <entity_type>:
+    - id: one
+    - id: two
+```
+
+In which `<change_type>` is one of modify, remove, or add and `<entity_type>` is one of the ESPHome entity types, which are for example `select`, `sensor` and `number`. For `modify` it is required that keys `id` and `newid` are present, where `id` refers to the original `id` in `source/heatpump-base.yaml`. In the resulting model file, the `id` field will be replaced with `newid`.
+
+### Example model file
+
+```yaml
+modify:
+  sensor:
+    - platform: template
+      id: compressor_starts_per_hour
+      newid: "{devicename}_coffee cups per month"
+      name: "Number of coffee cups per month"
+      unit_of_measurement: "cups"
+
+remove:
+  number:
+    - id: number123
+    - id: "${devicename}_set_water_temperature_t1s_zone_1"
+    - id: "${devicename}_dt1s5"
+  sensor:
+    - id: sensor123
+  select:
+    - id: "${devicename}_operational_mode"
+
+add:
+  sensor:
+    - plaform: modbus_controller
+      id: "${devicename}_extra_temp_sensor"
+      address: 250
+      name: "Extra Temp Sensor"
+      value_type: "U16"
+      unit_of_measurement: "°C"
+      device_class: "temperature"
+      accuracy_decimals: 1
+```
+
+## Creating a pull/merge request
+
+Pull requests are very welcome and it would be perfect if your pull request also updates the `CHANGELOG.md` to describe your changes.
---
+++ model-generator.py
@@ -0,0 +1,127 @@
+import os
+import glob
+import copy
+import re
+from ruamel.yaml import YAML
+from ruamel.yaml.scalarstring import LiteralScalarString
+from ruamel.yaml.compat import StringIO
+from ruamel.yaml.comments import CommentedMap
+
+
+def remove_comments(input):
+    output = ''
+
+    for line in input.splitlines(True):
+        if not re.match('^ +#', line):
+            output += line
+
+    return output
+    
+
+def load_yaml(file_path):
+    yaml = YAML()
+    yaml.preserve_quotes = True
+    yaml.indent(mapping=2, sequence=4, offset=2)
+
+    with open(file_path, 'r') as f:
+        yaml_data =  yaml.load(f)
+
+    if yaml_data is None:
+        return {}
+    else:
+        return yaml_data
+
+def save_yaml(data, filename):
+    yaml = YAML()
+    #yaml.preserve_quotes = True
+    yaml.indent(mapping=2, sequence=4, offset=2)
+    yaml.width = 4096
+    yaml.default_flow_style = False
+
+    # Patch None values to be dumped as empty scalars (key:)
+    def none_representer(dumper, _):
+        return dumper.represent_scalar('tag:yaml.org,2002:null', '')
+
+    yaml.representer.add_representer(type(None), none_representer)
+
+    with open(filename, 'w', encoding='utf-8') as f:
+        yaml.dump(data, f, transform=remove_comments)
+
+
+def apply_overrides(base_data, overrides={}):
+    base = copy.deepcopy(base_data)
+    remove_identifiers = []
+
+    # REMOVE per component type
+    if 'remove' in overrides:
+        for component_type, remove_items in overrides['remove'].items():
+            for remove_item in remove_items:
+                remove_identifiers.append(remove_item['id'])
+            if not isinstance(remove_identifiers, list):
+                print(f"Error: 'remove' section for '{component_type}' must be a list of identifiers.")
+                continue
+            if component_type in base and isinstance(base[component_type], list):
+                base[component_type] = [item for item in base[component_type] if item.get('id') not in remove_identifiers]
+
+    # MODIFY per component type
+    if 'modify' in overrides:
+        for component_type, modify_identifiers in overrides['modify'].items():
+            if not isinstance(modify_identifiers, list):
+                print(f"Error: 'modify' section for '{component_type}' must be a list.")
+                continue
+            if component_type not in base or not isinstance(base[component_type], list):
+                print(f"Warning: component type '{component_type}' not found in base.")
+                continue
+            for mod in modify_identifiers:
+                entity_id = mod.get('id')
+                modified = False
+                for item in base[component_type]:
+                    if item.get('id') == entity_id:
+                        item_index = base[component_type].index(item)
+                        mod['id'] = mod['newid']
+                        mod.pop('newid', None)
+                        base[component_type].remove(item)
+                        base[component_type].insert(item_index, mod)
+                        modified = True
+                        break
+                if not modified:
+                    print(f"Warning: id {entity_id} not found to modify in '{component_type}'.")
+
+    # ADD per component type
+    if 'add' in overrides:
+        for component_type, items in overrides['add'].items():
+            if not isinstance(items, list):
+                print(f"Error: 'add' section for '{component_type}' must be a list.")
+                continue
+            base.setdefault(component_type, [])
+            existing_addresses = {item.get('id') for item in base[component_type]}
+            for new_item in items:
+                addr = new_item.get('id')
+                if addr in existing_addresses:
+                    print(f"Warning: id {id} already exists in '{component_type}', skipping.")
+                else:
+                    base[component_type].append(new_item)
+
+    return base
+
+def main():
+    base_file = 'source/heatpump-base.yaml'
+    override_dir = 'source/models'
+    output_dir = 'models'
+
+    os.makedirs(output_dir, exist_ok=True)
+
+    base_data = load_yaml(base_file)
+
+    override_files = glob.glob(os.path.join(override_dir, '*.yaml'))
+
+    for override_file in override_files:
+        model_name = os.path.splitext(os.path.basename(override_file))[0]
+        overrides = load_yaml(override_file)
+        merged_data = apply_overrides(base_data, overrides)
+        output_file = os.path.join(output_dir, f'{model_name}.yaml')
+        save_yaml(merged_data, output_file)
+        print(f'Created: {output_file}')
+
+if __name__ == '__main__':
+    main()

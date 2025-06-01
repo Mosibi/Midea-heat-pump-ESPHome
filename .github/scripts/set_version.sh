2a797ac36291ca8d19467ef3c9a89032fb6bb9ef
Richard Arends <github@mosibi.nl>
2025-06-01 20:09:36 +0200

    Update github action (#94)

--- .github/scripts/set_version.sh
+++ .github/scripts/set_version.sh
@@ -2,7 +2,7 @@
 
 # This script extracts the new version from the
 # file CHANGELOG.md and uses that version to update
-# .esphome.project.version in heatpump.yaml
+# .esphome.project.version in source/heatpump-base.yaml
 
 
 function Message() {
@@ -27,12 +27,13 @@ function Error() {
   exit 101
 }
 
+BASE_YAML="source/heatpump-base.yaml"
 VERSION_FROM_CHANGELOG=$(grep '^## \[[0-9]' CHANGELOG.md | sed -E 's/^##\ \[([0-9].*)\]\ -.*/\1/g' | head -1)
-VERSION_IN_YAML=$(awk '/^ {4}version:/ {print $NF}' heatpump.yaml)
+VERSION_IN_YAML=$(awk '/^ {4}version:/ {print $NF}' ${BASE_YAML})
 
 if [[ ! -z "${VERSION_FROM_CHANGELOG}" ]] && [[ "${VERSION_FROM_CHANGELOG}" =~ ^[0-9].*$ ]]; then
   Info "Version extracted from changelog is ${VERSION_FROM_CHANGELOG}"
-  Info "Current version in heatpump.yaml is ${VERSION_IN_YAML}"
+  Info "Current version in ${BASE_YAML} is ${VERSION_IN_YAML}"
 
   if [[ "${VERSION_FROM_CHANGELOG}" == "${VERSION_IN_YAML}" ]]; then
     Info "Versions are the same, nothing has to be done now"
@@ -45,8 +46,8 @@ if [[ ! -z "${VERSION_FROM_CHANGELOG}" ]] && [[ "${VERSION_FROM_CHANGELOG}" =~ ^
   if [[ $? -eq 0 ]]; then
     Error "Version (tag) ${VERSION_FROM_CHANGELOG} already exists"
   else
-    sed -i -E "s/(^\ {4}version:)(.*$)/\1 ${VERSION_FROM_CHANGELOG}/g" heatpump.yaml || Error "Could not update version in heatpump.yaml"
-    Info "Version in heatpump.yaml is successful updated to ${VERSION_FROM_CHANGELOG}"
+    sed -i -E "s/(^\ {4}version:)(.*$)/\1 ${VERSION_FROM_CHANGELOG}/g" ${BASE_YAML} || Error "Could not update version in ${BASE_YAML}"
+    Info "Version in ${BASE_YAML} is successful updated to ${VERSION_FROM_CHANGELOG}"
     echo "YAML_UPDATED=true" >> $GITHUB_ENV
   fi
 else
